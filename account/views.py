c9c92320faea33981a2f55da27788f4b47bf5d14
Nedera <nghieped@gmail.com>
2023-01-22 20:23:46 +0700

    update

--- account/views.py
+++ account/views.py
@@ -30,16 +30,16 @@ class UserRegistrationView(APIView):
 class UserLoginView(APIView):
     def post(self, request, format=None):
         serializer = UserLoginSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            email = serializer.data.get('email')
-            password = serializer.data.get('password')
-            user = authenticate(email=email, password=password)
-            if user is not None:
-                token = get_tokens_for_user(user)
-                return Response({'token': token, 'msg': 'Login successful'}, status=status.HTTP_200_OK)
-            else:
-                return Response({'error': {'non_field_errors': ['Email or Password is not valid']}},
-                                status=status.HTTP_404_NOT_FOUND)
+        serializer.is_valid(raise_exception=True)
+        email = serializer.data.get('email')
+        password = serializer.data.get('password')
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            token = get_tokens_for_user(user)
+            return Response({'token': token, 'msg': 'Login successful'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': {'non_field_errors': ['Email or Password is not valid']}},
+                            status=status.HTTP_404_NOT_FOUND)
 
 
 class UserProfileView(APIView):
